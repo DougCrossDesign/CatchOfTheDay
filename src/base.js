218a7dfe101194de15ca3cfc5ba6358ac15ff5e6
DougCrossDesign <doug@dougcrossdesign.com>
2017-12-10 17:25:18 -0500

    Build

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDoMKSXxuWryS7BUlAopa1026NVeWUDRz4",
+  authDomain: "catch-of-the-day-doug-cross.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-doug-cross.firebaseio.com"
+});
+
+export default base;
